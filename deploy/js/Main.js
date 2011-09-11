bf0bce9073ffd9a6369374f5c3c792c65969ca94
Collin Hover <chover2@gmail.com>
2011-09-11 17:48:28 +0000

    restructured and added main script files

---
+++ deploy/js/Main.js
@@ -0,0 +1,14 @@
+define(["lib/jquery-1.6.2.min", 
+        "lib/signals.min", 
+        "lib/requestAnimFrame", 
+        "lib/requestInterval", 
+        "lib/requestTimeout", 
+        "utils/Error", 
+        "utils/Shared", 
+        "utils/Dev"], 
+    function() {
+        alert("main");
+        // return an object to define module
+        return {};
+    }
+);
