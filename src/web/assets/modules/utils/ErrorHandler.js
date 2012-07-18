fd9a40f5548b1360197dc3867c3e8733f87a9a21
collinhover <collinhover@gmail.com>
2012-07-18 18:31:20 -0500

    converted old shape system to polyominos, fixed error handling, added basic shape selection navigation

--- src/web/assets/modules/utils/ErrorHandler.js
+++ src/web/assets/modules/utils/ErrorHandler.js
@@ -13,33 +13,11 @@
         _ErrorHandler = {},
         errorState = false,
         errorCurrent = {},
-        errorHash = 'error=',
-        errorTypes = {
-            general: {
-                header: "Well, that was embarrasing.",
-                explanation: "We're very sorry about the error, but any progress you've made has been saved. <a href='" + shared.originLink + "'>All you need to do is reload the page.</a>"
-            },
-            webgl_browser: {
-                header: "Oops! We need WebGL!",
-                explanation: "We are sorry, but it appears that your browser does not support WebGL. For more information visit <a href='http://get.webgl.org' target='_blank'>Get WebGL</a> or try upgrading to one of these friendly browsers:",
-                browser_extra: true
-            },
-            webgl_other: {
-                header: "Oops! We need WebGL!",
-                explanation: "Although your browser seems to support WebGL, it appears as if your computer does not. For more information visit the <a href='http://get.webgl.org/troubleshooting/' target='_blank'>WebGL troubleshooting</a> page."
-            },
-            fourohfour: {
-                header: "We can't seem to find that page.",
-                explanation: "Sorry, but the page you were trying to view does not exist. This is probably the result of either a mistyped address or an out-of-date link."
-            },
-            forbidden: {
-                header: "We're afraid we can't let you do that.",
-                explanation: "Sorry, but the page you were trying to view is locked or hidden for a reason we can't tell you."
-            }
-        },
-        webglNames = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"],
-        browser_html = "<div class='browsers_each chrome'><a href='http://www.google.com/chrome/' target='_blank'>Chrome</a></div><div class='browsers_each firefox'><a href='http://www.mozilla.org/firefox/' target='_blank'>Firefox</a></div><div class='browsers_each safari'><a href='http://www.apple.com/safari/' target='_blank'>Safari</a></div>";
-    
+        errorStringBase = 'error',
+		errorTypeGeneral = 'General',
+        errorTypes = [ errorTypeGeneral, 'WebGLBrowser', 'WebGLComputer' ],
+        webglNames = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
+	
     /*===================================================
     
     public properties
@@ -73,33 +51,47 @@
         var canvas, context, errorType, i, l;
         
         // webgl browser check
-        if (!window.WebGLRenderingContext) {
-            errorType = 'webgl_browser';
+        if ( !window.WebGLRenderingContext ) {
+			
+            errorType = 'WebGLBrowser';
+			
         }
         else {
+			
             canvas = document.createElement( 'canvas' );
             
             // try each browser's webgl type
             for (i = 0, l = webglNames.length; i < l; i += 1) {
+				
                 try {
+					
                     context = canvas.getContext(webglNames[i]);
+					
                 }
                 catch ( e ) {
                 }
+				
                 if (context !== null && typeof context !== 'undefined') {
+					
                     break;
+					
                 }
+				
             }
             
             // if none found, there is another problem
-            if (context === null || typeof context === 'undefined') {
-                errorType = 'webgl_other';
+            if ( context === null || typeof context === 'undefined') {
+				
+                errorType = 'WebGLComputer';
+				
             }
         }
         
         // if error found, flag
         if (typeof errorType !== 'undefined') {
+			
             flag(errorType);
+			
         }
     }
     
@@ -119,7 +111,7 @@
     // flag error
     function flag ( errorType ) {
         if (typeof errorType !== 'undefined') {
-            window.location.hash = errorHash + errorType;
+            window.location.hash = errorStringBase + '=' + errorType;
         }
     }
     
@@ -128,11 +120,11 @@
         var hashError, hashErrorIndex;
         
         // check url hash for error message
-        hashError = window.location.hash.toString().replace(/#/, '');
-        hashErrorIndex = hashError.indexOf(errorHash);
+        hashError = window.location.hash.toString().replace( /#/, '', 1 );
+        hashErrorIndex = hashError.indexOf( errorStringBase );
         if (hashErrorIndex != -1) {
             // get error type
-            errorCurrent.type = hashError.replace(/error=/i, '');
+            errorCurrent.type = hashError.replace( errorStringBase + '=', '', 1 );
             
             // set error state
             errorState = true;
@@ -161,7 +153,7 @@
             history.pushState( { "pState": shared.originLink }, '', shared.originLink );
             
             // trigger shared error signal
-            shared.signals.error.dispatch(errorCurrent.type, origin || 'Unknown Origin', lineNumber || 'N/A' );
+            shared.signals.error.dispatch( errorCurrent.type, origin || 'Unknown Origin', lineNumber || 'N/A' );
         }
     }
     
@@ -181,82 +173,38 @@
     
     // show error to user
     function show ( error, origin, lineNumber ) {
-        var errorType, header, explanation, nerdtalk, extra, article, articleHeight, animSpeed = 500;
+		
+        var errorType, $element;
         
-        // does id not match a specific error
-        if ( errorTypes.hasOwnProperty( error ) ) {
-           errorType = error;
+        // if error type in list
+		
+        if ( errorTypes.indexOf( error ) !== -1 ) {
+			
+			errorType = error;
+			
         }
+		// else use general type
 		else {
-			errorType = 'general';
-		}
-    
-        // header
-        header = document.createElement('header');
-		$(header).addClass( 'title_alt text_huge' );
-        $(header).html(errorTypes[errorType].header);
-        
-        // explanation
-        explanation = document.createElement('p');
-        $(explanation).addClass("error_explanation");
-        $(explanation).html(errorTypes[errorType].explanation);
-        
-        // extra
-        if (errorTypes[errorType].browser_extra === true) {
-            extra = document.createElement('div');
-            $(extra).addClass("browsers");
-            $(extra).html(browser_html);
-        }
-        
-        // article
-        article = document.createElement('article');
-        $(article).addClass("error info_panel unselectable");
-        $(article).attr('id', errorType);
-        
-        // add to display
-        $(article).append(header);
-        $(article).append(explanation);
-        shared.domElements.$errors.append(article);
-        
-        // set height and negative margin-top
-        // no need to position, css top/left at 50%
-        articleHeight = $(header).outerHeight() + $(explanation).outerHeight();
-        
-        // append extra if needed
-        if(typeof extra !== 'undefined') {
-            $(article).append(extra);
-            articleHeight = articleHeight + $(extra).outerHeight();
-            $(extra).fadeOut(0).fadeIn(animSpeed);
-        }
-		
-		// error message, origin, and line number
-		
-		if ( typeof origin  !== 'undefined' && typeof lineNumber !== 'undefined' ) {
 			
-			nerdtalk = document.createElement('p');
-			$(nerdtalk).addClass("nerdtalk");
-			$(nerdtalk).html( "the error was -> '" + error + "' in " + origin + " at line # " + lineNumber);
-			$(article).append(nerdtalk);
-			articleHeight = articleHeight + $(nerdtalk).outerHeight();
-            $(nerdtalk).fadeOut(0).fadeIn(animSpeed);
+			errorType = errorTypeGeneral;
 			
 		}
-        
-        // fade and slide smoothly to new values
-        $(header).fadeOut(0).fadeIn(animSpeed);
-        $(explanation).fadeOut(0).fadeIn(animSpeed);
-        $(article).animate({
-            'height': articleHeight, 
-            'margin-top': Math.round(-articleHeight * 0.5)
-        }, animSpeed);
+		
+		// find dom element
+		
+		$element = $( "#" + errorStringBase + errorType );
+		
+		// show
+		
+		main.dom_collapse( {
+			element: $element,
+			show: true
+		} );
         
         // store
-        errorCurrent.domElements = {
-            article: article, 
-            header: header, 
-            explanation: explanation,
-            extra: extra
-        };
+		
+        errorCurrent.$element = $element;
+		
     }
     
 } ( KAIOPUA ) );
